f65fef4a4d4929eaaeaaecd910b5ac6235eb1f62
cristianbg11 <20151256@ce.pucmm.edu.do>
2020-10-09 13:25:52 -0400

    Agregando server

---
+++ evento_servicio/src/main/java/com/pucmm/evento_servicio/EventoServicioApplication.java
@@ -0,0 +1,13 @@
+package com.pucmm.evento_servicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoServicioApplication.class, args);
+	}
+
+}
---
+++ evento_servicio/src/test/java/com/pucmm/evento_servicio/EventoServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucmm.evento_servicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
